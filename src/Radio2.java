019a8409dd658481444bb7e658bc9ae004b6eb5c
liyangpan <liyangpan@catarc.ac.cn>
2019-03-02 14:45:27 +0800

    增加Radio接口

---
+++ src/Radio2.java
@@ -0,0 +1,11 @@
+/**
+ * Created by LiYangpan on 2019-03-02  14:44.
+ * with INTELLIJ IDEA on rmbp osx 10.11
+ * 描述:
+ */
+public interface Radio2 {
+
+    default int rrr(){
+        return 1;
+    }
+}
